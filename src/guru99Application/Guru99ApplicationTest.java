4fbb9be36643997498b6d5422776cf49633fefa4
AwadYMohammed <awadyasirawad@gmail.com>
2020-08-14 15:38:25 -0400

    Added a test

---
+++ src/guru99Application/Guru99ApplicationTest.java
@@ -0,0 +1,28 @@
+package guru99Application;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Guru99ApplicationTest {
+ChromeDriver driver;
+String url ="http://demo.guru99.com/v4/";
+
+
+
+@BeforeMethod
+public void invokeBrowser() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\awady\\Downloads\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get(url);
+	
+}
+
+@Test
+public void verifyTitleOfThePage() {
+	String expectedTitle = "Guru99 Bank Home Page";
+	String actualTitle = driver.getTitle();
+	Assert.assertEquals(actualTitle, expectedTitle);
+}}
