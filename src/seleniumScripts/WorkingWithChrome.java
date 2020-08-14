81146997cd4a895b3534aa5199ea19297040f2f0
AwadYMohammed <awadyasirawad@gmail.com>
2020-08-14 15:10:57 -0400

    Testing

---
+++ src/seleniumScripts/WorkingWithChrome.java
@@ -0,0 +1,31 @@
+package seleniumScripts;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WorkingWithChrome {
+	
+	ChromeDriver driver;
+	String url = "http://demo.guru99.com/";
+	public void invokeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\awady\\Downloads\\chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		
+	}
+public void getTitle() {
+	
+	String titleOfThePage = driver.getTitle();
+			System.out.println("Title of the page");
+}
+public void closeBrowser() {
+	driver.quit();
+}
+public static void main(String[] args) {
+	WorkingWithChrome wc = new WorkingWithChrome();
+	wc.invokeBrowser();
+	wc.getTitle();
+	wc.closeBrowser();
+}
+
+}
